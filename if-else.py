6c8b0a887a2d83b2ab1f44f83d8a9d2b73e9fa8e
Abdulkadir Tutar <124265160+AbdulkadirTUTAR@users.noreply.github.com>
2025-09-18 14:45:23 +0300

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,12 @@
+username = 'Kadir'
+password = '12345'
+
+
+if (username =='Kadir'):
+    if (password =='123445'):
+
+        print('Hoşgeldiniz')
+    else:
+        print('Parola yalış')
+else:
+    print('Username yanlış')
